b417e8c0260ed3cfbdb2933c9696350d0982184c
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-05-16 21:11:45 -0700

    first push with react scaffold

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import MainHeader from './main-header/main-header'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <MainHeader/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/main-header/main-header.js
@@ -0,0 +1,15 @@
+// import './_main-header.scss'
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+
+class MainHeader extends Component {
+    render() {
+        return (
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>
+        </header>
+        )
+    }
+}
